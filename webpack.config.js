f064f9a54169142c4bf550895ffe4bd8f367fe0d
Karan Sekhri <git@ksekhri.com>
2020-04-22 01:17:42 -0700

    Add html-webpack-plugin
    * Replace index.html with index.ejs

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
         test: /\.(js|ts|jsx|tsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.scss$/,
         use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
         ]
       },
       {
@@ -34,17 +35,40 @@ module.exports = {
       },
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
+  resolve: { extensions: ['*', '.js', '.jsx', '.ts', '.tsx'] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'js/bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, 'dist'),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    publicPath: '/',
+    hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
-};
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/index.ejs'),
+      scriptLoading: 'defer',
+      title: 'Page Title',
+      appMountId: 'root',
+      noscriptText: 'Requires JavaScript',
+      meta: [
+        {
+          name: 'description',
+          content: 'Search engine description',
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width',
+        },
+        {
+          charset: 'utf-8'
+        },
+      ],
+    })
+  ]
+}
